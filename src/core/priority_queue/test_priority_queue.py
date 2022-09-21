014d6a1c5dc50e8d6334eb5621b6426a43db037c
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-09-21 22:20:59 +0200

    Finish priority_queue tests

--- src/core/priority_queue/test_priority_queue.py
+++ src/core/priority_queue/test_priority_queue.py
@@ -11,11 +11,11 @@ def priority_queue():
 @pytest.fixture(autouse=True)
 def before_each(priority_queue):
     priority_queue.insert(2, (25, 34))
-    priority_queue.insert(4, (25, 34))
-    priority_queue.insert(0, (25, 34))
-    priority_queue.insert(10, (25, 34))
-    priority_queue.insert(3, (25, 34))
-    priority_queue.insert(8, (25, 34))
+    priority_queue.insert(4, (26, 34))
+    priority_queue.insert(0, (27, 34))
+    priority_queue.insert(10, (28, 34))
+    priority_queue.insert(3, (29, 34))
+    priority_queue.insert(8, (30, 34))
 
     yield
 
@@ -25,7 +25,7 @@ def test_length(priority_queue):
     assert len(priority_queue.h) == 6
 
 
-def test_insert_in_order_int_key(priority_queue):
+def test_insert_in_order(priority_queue):
 
     assert priority_queue.pop()[0] == 0
     assert priority_queue.pop()[0] == 2
@@ -54,3 +54,19 @@ def test_raise_empty_exception_pop(priority_queue):
     assert len(priority_queue.h) == 0
     with pytest.raises(EmptyQueueException):
         priority_queue.pop()
+
+
+def test_remove_top(priority_queue):
+    priority_queue.remove((27, 34))
+    assert len(priority_queue.h) == 5
+    assert priority_queue.top_key() == 2
+
+
+def test_remove_elements(priority_queue):
+    priority_queue.remove((28, 34))
+    priority_queue.remove((26, 34))
+    assert len(priority_queue.h) == 4
+    assert priority_queue.pop()[0] == 0
+    assert priority_queue.pop()[0] == 2
+    assert priority_queue.pop()[0] == 3
+    assert priority_queue.pop()[0] == 8
