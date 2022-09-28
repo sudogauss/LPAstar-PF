07afc166eaedd7dd3cc6b592eb624511492c07a6
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-09-28 22:38:41 +0200

    Init ros package for ROS2 portability. Create path finder ros interfaces. Init lpastar_pf ros node

---
+++ ros/ros2_ws/src/lpastar_pf/lpastar_pf/agent_client.py
@@ -0,0 +1,4 @@
+import rclpy
+from rclpy.node import Node
+from lpastar_pf.GAgent import GAgent
+from pf_interfaces.srv import Move, Position, Stop
---
+++ ros/ros2_ws/src/lpastar_pf/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'lpastar_pf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tim L.',
+    maintainer_email='t.liashkevich1772@gmail.com',
+    description='Path finder node based on LPA* algorithm using two clients: one for client\'s instructions communication and the other for scan data. It also publishes path\'s log messages.',
+    license='GNU GPLv3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
